87c2b675afe7947eea236e5994c145f457cf7a68
dvdblk <git@dvdblk.com>
2022-09-08 23:30:57 +0200

    Update immoscout24 author_icon_url

--- app/immo/website.py
+++ app/immo/website.py
@@ -11,6 +11,6 @@ class ImmoWebsite(Enum):
         """Return thumbnail URL depending on current enum value"""
         match self:
             case ImmoWebsite.IMMOSCOUT24:
-                return "https://play-lh.googleusercontent.com/FMd98MJtJLEo0uEDJtT8Gbs_fRjUV8aoVPpXcPTlZQwYL16vSh4XM2-y_X_-AYhQeMc"
+                return "https://play-lh.googleusercontent.com/9qHvJvi4zgrXzfhsDlJpyU2JEaadxVtLOy0zg3vUNQTAJhZFroCcXObWMEhlhf_sOkQ"
             case ImmoWebsite.HOMEGATE:
                 return "https://yt3.ggpht.com/ytc/AKedOLSoh7FW3igKCBh1866eXYyNt87wjZ4QJLXMvn3S5g=s900-c-k-c0x00ffffff-no-rj"
